636eb307c7b07cbf43692c25fe94dd204e2cd99e
PaulzSh <17302134335@163.com>
2025-03-28 16:56:44 +0800

    Updated swagger UI
    Updated swagger UI

--- LeavePlannerProject/src/main/resources/data.sql
+++ LeavePlannerProject/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 -- Only insert if table exists (H2 compatible)
-MERGE INTO leave_type (type_name, description) KEY(type_name) VALUES
+INSERT  INTO leave_type (type_name, description) KEY(type_name) VALUES
 ('Vacation', 'Annual paid leave'),
 ('Medical', 'Sick leave with medical certificate'),
 ('WFH', 'Work from home arrangement'),
